90be025548c27eb4c5f0ad18ac26b34d09403e99
Marcelo Lemos <marcelo.lemos@consensotec.com.br>
2025-08-25 16:44:18 -0300

    por favor funcionee

--- frontend/src/pages/CircuitoPage.jsx
+++ frontend/src/pages/CircuitoPage.jsx
@@ -134,27 +134,26 @@ const CircuitoPage = () => {
       )}
 
       {/* Vídeos Recentes */}
-      <section>
-        <h2 className="circuito-section-title">Últimos Confrontos</h2>
-        <div className="video-list">
-          {agendaCircuito
-            .filter(partida => partida.linkTransmissao)
-            .sort((a, b) => new Date(b.data) - new Date(a.data))
-            .slice(0, 2)
-            .map((partida, index) => (
-              <iframe
-                key={index}
-                src={formatEmbedLink(partida.linkTransmissao)}
-                title={`Confronto ${index + 1}`}
-                width="560"
-                height="315"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            ))}
-        </div>
-      </section>
+     <section>
+  <h2 className="circuito-section-title">Últimos Confrontos</h2>
+  <div className="video-list">
+    {agendaCircuito
+  .filter(partida => partida.linkTransmissao)
+  .sort((a, b) => new Date(b.data) - new Date(a.data))
+  .slice(0, 2)
+  .map((partida, index) => {
+    const link = partida.linkTransmissao.replace("watch?v=", "embed/");
+    return (
+      <iframe
+        key={index}
+        src={link}
+        title={`Confronto ${index + 1}`}
+        allowFullScreen
+      ></iframe>
+    );
+  })}
+  </div>
+</section>
 
       {/* Notícias */}
       <section>
