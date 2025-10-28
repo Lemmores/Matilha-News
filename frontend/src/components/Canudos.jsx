7ff2f8f87694696b791b12554f69f78bfa429842
Marcelo Lemos <marcelo.lemos@consensotec.com.br>
2025-10-28 17:36:22 -0300

    RED CANUDOSss testes

--- frontend/src/components/Canudos.jsx
+++ frontend/src/components/Canudos.jsx
@@ -4,11 +4,29 @@ import './Canudos.css';
 export default function RedCanudos() {
   useEffect(() => {
     document.body.classList.add('redcanudos-bg');
-
     return () => {
       document.body.classList.remove('redcanudos-bg');
     };
   }, []);
+
+  const times = [
+    { nome: 'Amantes da Nissinha', logo: '/amantes.jpg' },
+    { nome: 'Artic', logo: '/artic.jpg' },
+    { nome: 'Bigodes', logo: '/bigodes.jpg' },
+    { nome: 'Cabras', logo: '/cabras.jpg' },
+    { nome: 'Canabbis', logo: '/canabbis.jpg' },
+    { nome: 'Canalhas', logo: '/canalhas.jpg' },
+    { nome: 'Capangas', logo: '/capangas.jpg' },
+    { nome: 'Cuzão', logo: '/cuzão.jpg' },
+    { nome: 'Cuzinho', logo: '/cuzinho.jpg' },
+    { nome: 'F4N', logo: '/f4n.jpg' },
+    { nome: 'Foxtail', logo: '/foxtail.jpg' },
+    { nome: 'Masters', logo: '/masters.jpg' },
+    { nome: 'Minas', logo: '/minas.jpg' },
+    { nome: 'RED J', logo: '/redj.jpg' },
+    { nome: 'RED PE', logo: '/redpe.jpg' },
+    { nome: 'Renegados', logo: '/renegados.jpg' },
+  ];
 
   return (
     <div className="red-canudos">
@@ -30,12 +48,14 @@ export default function RedCanudos() {
 
       <h3>Times confirmados dessa edição:</h3>
 
-    <img
-    src='/Logos Canudos/amantes'
-    />
-
-    
-      
+      <div className="logos-times">
+        {times.map((time, index) => (
+          <div key={index} className="logo-card">
+            <img src={time.logo} alt={time.nome} className="logo-time" />
+            <p>{time.nome}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
