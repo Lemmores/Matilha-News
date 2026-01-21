3d328170046d86610122cd534a541803a7dbfc69
Marcelo Lemos <marcelo.lemos@consensotec.com.br>
2026-01-21 16:02:00 -0300

    atualizações cs2

--- frontend/src/pages/CSPage.jsx
+++ frontend/src/pages/CSPage.jsx
@@ -7,7 +7,7 @@ const jogadores = [
   { nome: 'HISTORY', img: '/jogadores/history.jpg', twitter: 'https://x.com/historyfps', instagram: 'https://www.instagram.com/historycsgo/' },
   { nome: 'DROP', img: '/jogadores/drop.jpg', twitter: 'https://x.com/dropcs__', instagram: 'https://www.instagram.com/dropcsgo_/' },
   { nome: 'KAUEZ', img: '/jogadores/kauez.jpg', twitter: 'https://x.com/kauezcs', instagram: 'https://www.instagram.com/kauezcsgo/' },
-  { nome: 'PONTER', img: '/jogadores/ponter.jpg', twitter: 'https://x.com/ponterzin', instagram: 'https://www.instagram.com/ponterzin/' },
+  { nome: 'CHAY', img: '/jogadores/chayJESUS.jpg', instagram: 'https://www.instagram.com/chayfps' },
   { nome: 'DAV1DEUS', img: '/jogadores/dav1deus.jpg', twitter: 'https://x.com/dav1deuS_', instagram: 'https://www.instagram.com/dav1deus_/' },
 ];
 
